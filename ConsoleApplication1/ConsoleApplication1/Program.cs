dcfad56b0535f969c0f94e11f5f16c781d16d34b
gg627933 <gg627933@gmail.com>
2016-05-01 18:45:01 +0100

    ffff

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -3,7 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
-
+using Microsoft.SqlServer.Dts.Runtime;
+using Microsoft.SqlServer.Management.IntegrationServices;
+using Microsoft.SqlServer.Management.Smo.Agent;
+using System.Data.SqlClient;
+using Microsoft.SqlServer.Management.Smo;
+using Microsoft.SqlServer.Server;
+using Microsoft.SqlServer.Management.Common;
 
 namespace ConsoleApplication1
 {
@@ -12,16 +18,18 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
 
-            //   File_Import f = new File_Import();
 
-            Console.WriteLine("Hello you");
+            SqlConnection conn = new SqlConnection("Data source=NIBS; Database=Books;User Id=GH;Password=GH");
+            SqlCommand cmd = new SqlCommand("IF EXISTS(select name from sysobjects where name = 'GH') BEGIN DROP TABLE GH END create table GH(empno int,empname varchar(50),salary money);", conn);
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            Console.WriteLine("Table Created Successfully...");
+            conn.Close();
+
 
             Console.ReadKey();
-
-
-
-
         }
+
 
 
 
